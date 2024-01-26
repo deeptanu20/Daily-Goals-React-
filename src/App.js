0daac693dc43f168714c88aaf8715e36879ca1a8
Deeptanu Bhatta <deeptanubhatta2002@gmail.com>
2024-01-26 20:00:11 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "../src/components/Header"
+import Home from "../src/components/Home";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Header = () => {
+  return <nav>Get Ready To Complete Your Daily Goals</nav>;
+};
+
+export default Header;
+
